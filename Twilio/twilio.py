93a84529c08d5e1b8887ff9a4ab264e0105d4c09
Lightmingvl <lightmingvl@gmail.com>
2016-04-12 23:16:36 +0300

    Added files via upload

---
+++ Twilio/twilio.py
@@ -0,0 +1,11 @@
+from twilio.rest import TwilioRestClient #pipinstall twilio
+
+Account_SID = "ACb8e28dc4a841b1204a3ab6d957c7cbbf"
+Auth_Token = "8a5bde354ffa747c950b18b8477ebaca"
+client = TwilioRestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+client.message.create(
+    to = "+380504571301",
+    from = "+12674335886",
+    body = "python sender",
+    )
+print (message)

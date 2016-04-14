c005f5a07c3c415911f5b3093bdc495a7778a55c
Lightmingvl <lightmingvl@gmail.com>
2016-04-14 15:31:38 +0300

    Update twiter.py

--- tweepy/twiter.py
+++ tweepy/twiter.py
@@ -1,10 +1,10 @@
 import tweepy, time, sys
 argfile = str(sys.argv[1])
   
-CONSUMER_KEY = "qMChojDsZ6J6qGlViY1tHvvdJ"
-CONSUMER_SECRET = "rmSlUG0JI9K5x8rVJdcqpKGjCjUMJ7B1y7i5jj6SS1JLZ9PESN"
-ACCESS_KEY = "395543545-NiwKwFC3IlA8WtaKQ4aeRaCP1pbs5KiALOy0NNF3" # Access Token
-ACCESS_SECRET = "jsgNJsJrXcXyryttFw9mO5ZhFNXdGB98bQpk2Hb8EQHOE" # Access secret
+CONSUMER_KEY = "xxxxxxxxxxxxx"
+CONSUMER_SECRET = "zzzzzzzzzzzzzzz"
+ACCESS_KEY = "ccccccccccccccccccccccccc" # Access Token
+ACCESS_SECRET = "vvvvvvvvvvvvvvvvvvvvvvvvv" # Access secret
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
